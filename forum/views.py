a420ddf711218299cebc12bc8b43cb44bdf8f36f
kingtroga <trogaclassicman@gmail.com>
2024-04-20 22:04:24 +0100

    Done with forum index page pagination and look

--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Q
 from . import models
+from django.core.paginator import Paginator
 
 def index(request):
     query = request.GET.get('query', '')
@@ -8,10 +9,18 @@ def index(request):
 
     if query:
         forums = forums.filter(Q(title__icontains=query) | Q(content__icontains=query))
+
+
+    paginator = Paginator(forums, 6) 
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+   
         
 
     return render(request, 'forum/forum_index.html',
                   {"forums": forums,
-                   'query': query,})
+                   'query': query,
+                   "page_obj": page_obj})
 
 
