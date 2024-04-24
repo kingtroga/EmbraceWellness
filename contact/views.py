fe174e2cfda8c92e6a0739814c7fb40680f0e81a
kingtroga <trogaclassicman@gmail.com>
2024-04-24 14:44:02 +0100

    dm template achieved

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Contact
 
-# Create your views here.
+def index(request):
+    user_is_staff = request.user.is_staff
+    return render(request, 'contact/contact_index.html', {
+        'user_is_staff': user_is_staff
+    })
