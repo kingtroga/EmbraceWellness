0a3ced6bf96af23192e85a1ec9449f30789c72d3
kingtroga <trogaclassicman@gmail.com>
2024-04-24 18:14:32 +0100

    almost done

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Contact
 from .forms import AttendToForm
+from django.contrib.auth.decorators import user_passes_test
 
+def is_staff(user):
+    return user.is_staff
+
+@user_passes_test(is_staff)
 def index(request):
     user_is_staff = request.user.is_staff
     direct_messages = Contact.objects.filter(attended_to=False).all()
@@ -15,6 +20,7 @@ def index(request):
         'direct_messages': direct_messages,
     })
 
+@user_passes_test(is_staff)
 def detail(request, pk):
     message = get_object_or_404(Contact, pk=pk)
     user_is_staff = request.user.is_staff
