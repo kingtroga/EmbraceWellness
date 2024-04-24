c41d7f60fe9fe5a53ab3fe2c08c6eaca1d804f9f
kingtroga <trogaclassicman@gmail.com>
2024-04-24 16:26:59 +0100

    created the detail for dms

--- contact/urls.py
+++ contact/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:pk>/', views.detail, name='detail'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Contact
 
 def index(request):
     user_is_staff = request.user.is_staff
-    direct_messages = Contact.objects.all()
+    direct_messages = Contact.objects.filter(attended_to=False).all()
 
 
     for direct_message in direct_messages:
@@ -13,3 +13,13 @@ def index(request):
         'user_is_staff': user_is_staff,
         'direct_messages': direct_messages,
     })
+
+def detail(request, pk):
+    message = get_object_or_404(Contact, pk=pk)
+    user_is_staff = request.user.is_staff
+
+    return render(request, 'contact/detail.html', {
+        'user_is_staff': user_is_staff,
+        'message': message,
+    })
+
